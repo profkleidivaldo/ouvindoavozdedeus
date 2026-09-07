a7b8f9956ed53af294f5c6372465a9aecf067ce6
profkleidivaldo <profkleidivaldo@gmail.com>
2026-09-07 09:29:25 -0400

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 // URL da implantação do Google Apps Script (Web App).
 // Não coloque a chave da Mistral aqui. A chave fica protegida no Script Properties do Apps Script.
-const IA_APPS_SCRIPT_URL = "COLE_AQUI_A_URL_DO_SEU_APPS_SCRIPT_/exec";
+const IA_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwNTJWHixBSPCIOXDZs8xj9rVQoW_g5LVp2l-BlIzODlUJqz4q1oWrncACm75v5DxT-6Q/exec";
